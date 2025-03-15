b95c484aca29eb14452274854d22512a6585024b
JuLian0705 <131489864+JuLian0705@users.noreply.github.com>
2025-03-15 14:24:06 -0500

    Corregido popup para que aparezca después del loader

--- js/pupo.js
+++ js/pupo.js
@@ -1,7 +1,15 @@
 function showSuccessPopup() {
-    document.getElementById("successPopup").style.display = "flex";
-  }
+    // Mostrar el loader primero
+    document.getElementById("loadingSpinner").style.display = "flex";
 
-  document.querySelector(".close-popup").addEventListener("click", function() {
+    // Esperar 5 segundos antes de mostrar el popup
+    setTimeout(() => {
+        document.getElementById("loadingSpinner").style.display = "none"; // Ocultar loader
+        document.getElementById("successPopup").style.display = "flex"; // Mostrar popup
+    }, 5000);
+}
+
+// Cerrar el popup cuando se hace clic en la "X"
+document.querySelector(".close-popup").addEventListener("click", function() {
     document.getElementById("successPopup").style.display = "none";
-  });
+});
